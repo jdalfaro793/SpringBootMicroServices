5de3c92b5eb805a0771131eb7fd68004cb2f05e8
Josué Darío Alfaro <darioalfarounju@gmail.com>
2025-01-22 10:26:34 -0300

    dockerizando el proyecto para trabajar por containers

--- SpringMicroservices/microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ SpringMicroservices/microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservice.course.dto.StudentDTO;
 
-@FeignClient(name = "msvc-student", url="localhost:8098/api/student")
+@FeignClient(name = "microservice-student", url = "http://microservice-student:8090/api/student")
 public interface StudentClient {
 	
 	@GetMapping("/search-by-course/{id}")
